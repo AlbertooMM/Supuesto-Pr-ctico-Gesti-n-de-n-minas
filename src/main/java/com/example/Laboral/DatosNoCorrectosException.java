b75fe9bfda9fb980319ea9adfef660b0855c699c
AlbertooMM <ertortu2006@gmail.com>
2026-09-21 12:53:53 +0200

    salud 2

---
+++ src/main/java/com/example/Laboral/DatosNoCorrectosException.java
@@ -0,0 +1,18 @@
+package Laboral;
+
+public class DatosNoCorrectosException extends Exception {
+
+    private String Message;
+
+    public DatosNoCorrectosException(String mensaje) {
+        mensaje = Message;
+    }
+
+    /** 
+     * @return String
+     */
+    public String getMessage() {
+        return Message;
+
+    }
+}
---
+++ src/main/java/com/example/Laboral/Empleado.java
@@ -0,0 +1,55 @@
+package Laboral;
+
+public class Empleado extends Persona {
+
+    public int categoria;
+    public int anyos;
+
+    public Empleado(String nombre, String dni, char sexo) throws DatosNoCorrectosException {
+        super(nombre, dni, sexo);
+        categoria = 1;
+        anyos = 0;
+
+    }
+
+    public Empleado(String nombre, String dni, char sexo, int categoria, int anyos) throws DatosNoCorrectosException {
+        super(nombre, dni, sexo);
+        if (anyos > 0) {
+            this.categoria = categoria;
+            this.anyos = anyos;
+        }
+        else{
+            throw new  DatosNoCorrectosException("Datos no Correctos");
+
+        }
+    }
+
+    /** 
+     * @param newcategoria
+     */
+    public void setCategoria(int newcategoria) {
+        if (newcategoria >= 1 && newcategoria <= 10) {
+            categoria = newcategoria;
+        }
+
+    }
+
+    /** 
+     * @return int
+     */
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void incrAnyo() {
+        anyos += 1;
+    }
+
+    @Override
+    public void imprime() {
+        super.imprime();
+        System.out.println("Categoria: " + categoria);
+        System.out.println("Años: " + anyos);
+
+    }
+}
